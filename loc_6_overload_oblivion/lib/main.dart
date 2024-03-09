99f3bcb0bb696c97e58a8905191d797b3b52e82c
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-03-09 11:29:29 +0530

    Adding initialization in main.dart

--- loc_6_overload_oblivion/lib/main.dart
+++ loc_6_overload_oblivion/lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:loc_6_overload_oblivion/firebase_options.dart';
 import 'package:loc_6_overload_oblivion/staff_login.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -9,8 +13,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:LoginPage()
-    );
+    return MaterialApp(home: LoginPage());
   }
 }
